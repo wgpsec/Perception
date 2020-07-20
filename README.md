4f1d9c7657767b62d5fc46154c9a05601514207f
Abao520 <1574991635@qq.com>
2020-07-20 21:10:18 +0800

    :rocket:​ 第一次提交

---
+++ README.md
@@ -0,0 +1,55 @@
+# 为什么会有这个东西
+
+
+
+
+
+# Use
+
+首先需要我们安装几个第三方库：
+
+```python
+pip install requests
+pip install demjson
+pip insatll argparse
+```
+
+第一次使用需要输入一下社区的`cookie`，可以先输入 `--help`进行添加,，然后会在当前的目录下生成一个`cookies.txt`文件。下面是使用的一些参数：
+
+```
+usage: test.py [-h] [--c C] [--h H] [--t T] [--p P]
+
+嗨！你好！ 当你看到这里的时候，很高兴你已经成为了我们的一员！ 当你第一次运行的时候需要你输入一次你的cookie，方便我们认证，只需要一次哦！
+
+optional arguments:
+  -h, --help  show this help message and exit
+  --c C       查询的城市
+  --h H       查询的IP
+  --t T       查询网站标题
+  --p P       查询开放特定端口
+```
+
+目前仅开发出`-C`和`-h` 
+
+# eg
+
+`python test.py --c 成都`
+
+![image-20200720204756131](demo.png)
+
+目前仅支持域名的查找，后续功能会逐渐添加
+
+## Furter
+
+在未来的开发中，会逐渐完善以下功能：
+
+* ~~查看基本的域名信息~~
+
+* 查找对应的指纹和服务
+* 查找开放的常见端口
+* 和[知识库](https://plat.wgpsec.org/knowledge)对接，便于查看对应的指纹POC和文献啥
+* ......
+
+# 免责声明
+
+不能使用该工具进行非法活动，下载该工具就表示同意此条款，后续与作者无关
---
+++ scan.py
@@ -0,0 +1,164 @@
+import requests
+import demjson
+import argparse
+import sys
+import os
+
+print(
+'==========================================================================================\n'
+'||                                                                                      ||\n'
+'||                                                                                      ||\n'
+'||  ██       ██                  ████████                                               ||\n'                                          
+'|| ░██      ░██  █████  ██████  ██░░░░░░                                       █████    ||\n' 
+'|| ░██   █  ░██ ██░░░██░██░░░██░██         █████   █████       ██████  ██████ ██░░░██   ||\n'
+'|| ░██  ███ ░██░██  ░██░██  ░██░█████████ ██░░░██ ██░░░██     ██░░░░██░░██░░█░██  ░██   ||\n'
+'|| ░██ ██░██░██░░██████░██████ ░░░░░░░░██░███████░██  ░░     ░██   ░██ ░██ ░ ░░██████   ||\n'
+'|| ░████ ░░████ ░░░░░██░██░░░         ░██░██░░░░ ░██   ██    ░██   ░██ ░██    ░░░░░██   ||\n'
+'|| ░██░   ░░░██  █████ ░██      ████████ ░░██████░░█████  ░██ ░░██████ ░███     █████   ||\n'
+'|| ░░       ░░  ░░░░░  ░░      ░░░░░░░░   ░░░░░░  ░░░░░   ░░  ░░░░░░  ░░░     ░░░░░     ||\n'
+'||                                                                                      ||\n'
+'||                                From www.WgpSec.org                                   ||\n'
+'||                           在线信息收集工具 V1.0 By Abao520                              ||\n'
+'==========================================================================================\n'
+)
+# 提示信息
+parser = argparse.ArgumentParser()
+parser.description=('嗨！你好！\n'
+                    '当你看到这里的时候，很高兴你已经成为了我们的一员！\n'
+                    '当你第一次运行的时候需要你输入一次你的cookie，方便我们认证，只需要一次哦！')
+
+parser.add_argument("--c", type=str,help="查询的城市")
+parser.add_argument("--h", type=str,help="查询的IP")
+parser.add_argument("--t", type=str,help="查询网站标题")
+parser.add_argument("--p", type=str,help="查询开放特定端口")
+args = parser.parse_args()
+
+#判断cookie
+
+path = os.getcwd()
+cookie_txt = os.path.exists('cookies.txt')
+if cookie_txt == False:
+    with open('cookies.txt','w') as f:
+        cookies_input = input('你还没有cookie，请先输入你的cookie值：')
+        f.write(cookies_input)
+        f.close()
+else:
+    pass
+
+with open('cookies.txt','r+') as f:
+    cookies_read = f.read()
+    f.close()
+
+class Search(object):
+
+    def __init__(self):
+        pass
+
+    # 城市查询
+    def city(self,city,pageSize=20):
+        api_url = 'https://plat.wgpsec.org/api/v1/ws/search'
+        key = ['id', 'subdomain', 'ipAdd', 'host', 'cdn', 'city', 'status', 'dnsType', 'subdomainLevel',
+               'subdomainTitle',
+               'subdomainBanner', 'subdomainReason', 'subdomainStatus', 'addTime', 'wsUrlsInfoDto']
+        header = {
+            "Cookie":cookies_read
+        }
+        json = {
+            "pageNo": 1,
+            "pageSize": pageSize,
+            "query": "city=%s" %city,
+            "type": "web"
+        }
+        api_reposen = requests.post(api_url, headers=header, json=json)
+        api_reposen_json = demjson.decode(api_reposen.text)
+        api_data = api_reposen_json['data']  # 提取返回中的data值
+        wsPortInfoDtoList = api_data['wsPortInfoDtoList']  # 端口开放情况
+        print('开放的端口信息为：%s' % wsPortInfoDtoList)
+        wsSubDomainInfoDtoList = api_data['wsSubDomainInfoDtoList']
+        wsSubDomainInfoDtos = wsSubDomainInfoDtoList['wsSubDomainInfoDtos']  # 查询出的域名和子域
+        i = 0
+        try:
+            while i < pageSize:  # 遍历出我们插到的信息
+                print('域名：%s' % wsSubDomainInfoDtos[i][key[1]])
+                i += 1
+        except IndexError:
+
+            print('查询出错')
+        finally:
+            print('查询成功')
+    # 域名查询
+    def host(self,host,pageSize=20):
+        api_url = 'https://plat.wgpsec.org/api/v1/ws/search'
+        key = ['id', 'subdomain', 'ipAdd', 'host', 'cdn', 'city', 'status', 'dnsType', 'subdomainLevel',
+               'subdomainTitle',
+               'subdomainBanner', 'subdomainReason', 'subdomainStatus', 'addTime', 'wsUrlsInfoDto']
+        header = {
+            "Cookie":cookies_read
+        }
+        json = {
+            "pageNo": 1,
+            "pageSize": pageSize,
+            "query": "ip=%s" %host,
+            "type": "web"
+        }
+        api_reposen = requests.post(api_url, headers=header, json=json)
+        api_reposen_json = demjson.decode(api_reposen.text)
+        api_data = api_reposen_json['data']  # 提取返回中的data值
+        wsPortInfoDtoList = api_data['wsPortInfoDtoList']  # 端口开放情况
+        wsSubDomainInfoDtoList = api_data['wsSubDomainInfoDtoList']
+        wsSubDomainInfoDtos = wsSubDomainInfoDtoList['wsSubDomainInfoDtos']  # 查询出的域名和子域
+        i = 0
+        try:
+            while i < pageSize:  # 遍历出我们插到的信息
+                print('域名：%s' % wsSubDomainInfoDtos[i][key[1]])
+                i += 1
+        except IndexError:
+
+            print('')
+        finally:
+            print('查询成功')
+    # 标题查询
+    def title(self,title,pageSize=2):
+        api_url = 'https://plat.wgpsec.org/api/v1/ws/search'
+        key = ['id', 'subdomain', 'ipAdd', 'host', 'cdn', 'city', 'status', 'dnsType', 'subdomainLevel',
+               'subdomainTitle',
+               'subdomainBanner', 'subdomainReason', 'subdomainStatus', 'addTime', 'wsUrlsInfoDto']
+        header = {
+            "Cookie":cookies_read
+        }
+        json = {
+            "pageNo": 1,
+            "pageSize": pageSize,
+            "query": "title=%s" %title,
+            "type": "web"
+        }
+        api_reposen = requests.post(api_url, headers=header, json=json)
+        api_reposen_json = demjson.decode(api_reposen.text)
+        api_data = api_reposen_json['data']  # 提取返回中的data值
+        wsPortInfoDtoList = api_data['wsPortInfoDtoList']  # 端口开放情况
+        wsSubDomainInfoDtoList = api_data['wsSubDomainInfoDtoList']
+        wsSubDomainInfoDtos = wsSubDomainInfoDtoList['wsSubDomainInfoDtos']  # 查询出的域名和子域
+        i = 0
+        try:
+            while i < pageSize:  # 遍历出我们插到的信息
+                print('域名：%s' % wsSubDomainInfoDtos[i][key[1]])
+                i += 1
+        except IndexError:
+
+            print('')
+        finally:
+            print('查询成功')
+
+
+
+
+if args.c:
+    city_ = Search()
+    city_.city(args.c)
+if args.h:
+    host_ = Search()
+    host_.host(args.h)
+if args.t:
+    title_ = Search()
+    title_.host(args.t)
+
