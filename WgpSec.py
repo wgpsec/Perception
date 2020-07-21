400727625e4c4c3dadb9e4bdd0036a74e56576bd
Abao520 <1574991635@qq.com>
2020-07-21 10:50:08 +0800

    :point_up: 规范代码，添加注释

--- WgpSec.py
+++ WgpSec.py
@@ -18,10 +18,10 @@ print(
 '|| ░░       ░░  ░░░░░  ░░      ░░░░░░░░   ░░░░░░  ░░░░░   ░░  ░░░░░░  ░░░     ░░░░░     ||\n'
 '||                                                                                      ||\n'
 '||                                From www.WgpSec.org                                   ||\n'
-'||                                在线信息收集工具 V1.0                                 ||\n'
+'||                                在线信息收集工具 V1.0                                    ||\n'
 '==========================================================================================\n'
 )
-# 提示信息和外部参数的设置
+# 提示信息以及外部参数的接收
 parser = argparse.ArgumentParser()
 parser.description=('嗨！你好！\n'
                     '当你看到这里的时候，很高兴你已经成为了我们的一员！\n'
@@ -59,7 +59,7 @@ class Search(object):
         api_url = 'https://plat.wgpsec.org/api/v1/ws/search'
         key = ['id', 'subdomain', 'ipAdd', 'host', 'cdn', 'city', 'status', 'dnsType', 'subdomainLevel',
                'subdomainTitle',
-               'subdomainBanner', 'subdomainReason', 'subdomainStatus', 'addTime', 'wsUrlsInfoDto'] # 把json返回值的key值按顺序列出来
+               'subdomainBanner', 'subdomainReason', 'subdomainStatus', 'addTime', 'wsUrlsInfoDto']
         header = {
             "Cookie":cookies_read
         }
@@ -68,28 +68,34 @@ class Search(object):
             "pageSize": pageSize,
             "query": "city=%s" %city,
             "type": "web"
-        } # 请求信息
-        
+        }
         api_reposen = requests.post(api_url, headers=header, json=json)
         api_reposen_json = demjson.decode(api_reposen.text)
+
+        # json 返回值里面会有3个key分别是：code、msg、data
         api_data = api_reposen_json['data']  # 提取返回中的data值
-        wsPortInfoDtoList = api_data['wsPortInfoDtoList']  # 端口开放情况
-        
-        wsSubDomainInfoDtos = wsSubDomainInfoDtoList['wsSubDomainInfoDtos']  # 查询出的域名和子域，子域和域名都一起
+
+        # data下面有wsDomainInfoDto、wsSubDomainInfoDtoList、wsPortInfoDtoList、wsUrlsInfoDtoList 目前只有wsSubDomainInfoDtoList存在可用信息，其他返回值为null，所以其他的参数不写
+
+        # wsPortInfoDtoList = api_data['wsPortInfoDtoList']
+
+        wsSubDomainInfoDtoList = api_data['wsSubDomainInfoDtoList']
+
+        # wsSubDomainInfoDtoList中有5个key，分别是wsSubDomainInfoDtos、pageSize、totalCount、pageNo、pages。所有的域名信息都在wsSubDomainInfoDtos里面，包括子域名
+        wsSubDomainInfoDtos = wsSubDomainInfoDtoList['wsSubDomainInfoDtos']  # 查询出的域名和子域
         i = 0
         try:
-            while i < pageSize:  # 遍历出我们插到的信息，根据查询的pageSize进行比较
-                print('网站标题：：%s\n域名%s' % (wsSubDomainInfoDtos[i][key[9]],wsSubDomainInfoDtos[i][key[1]])) # 用下标取出
-                print(' ' * 100) 
+            while i < pageSize:  # 遍历出我们插到的信息
+                print('网站标题：：%s\n域名%s' % (wsSubDomainInfoDtos[i][key[9]],wsSubDomainInfoDtos[i][key[1]])) # 这里我用的是下标，可以优化一下
+                print(' ' * 100)
                 print(' ' * 100)
                 i += 1
-        except IndexError: # 捕获所有异常
+        except IndexError:
 
             print('查询出错')
         finally:
             print('查询成功')
-  
-  # 域名查询
+    # 域名查询
     def host(self,host,pageSize=20):
         api_url = 'https://plat.wgpsec.org/api/v1/ws/search'
         key = ['id', 'subdomain', 'ipAdd', 'host', 'cdn', 'city', 'status', 'dnsType', 'subdomainLevel',
@@ -148,7 +154,7 @@ class Search(object):
         try:
             while i < pageSize:  # 遍历出我们插到的信息
                 print('网站标题：：%s\n域名%s' % (wsSubDomainInfoDtos[i][key[9]], wsSubDomainInfoDtos[i][key[1]]))
-                print(' '*100)
+                print(' ' * 100)
                 print(' ' * 100)
 
                 i += 1
@@ -160,7 +166,7 @@ class Search(object):
 
 
 
-#外部输入的
+
 if args.c:
     city_ = Search()
     city_.city(args.c)
