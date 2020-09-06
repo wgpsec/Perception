48610eccd8902591658d9e2ef2530eca13e088d8
ro4lsc <2386788894@qq.com>
2020-09-06 22:08:52 +0800

    :sparkles: Perception v1.2

--- main.py
+++ main.py
@@ -11,16 +11,19 @@ def main():
 
     parser.add_argument('-l', '--login', help='登陆')
     parser.add_argument('-t', '--type', help='(web|host)', default='')
-    # parser.add_argument('-s', '--search', default='')
+    parser.add_argument('-u', '--url', help='需要添加扫描的域名')
     parser.add_argument('-q', '--query', help='(port|host|title|ip|city)', default='')
     parser.add_argument('-k', '--keyword', help='enable knowledge api', default='')
+    parser.add_argument('-e', '--export', help='查询结果保存的文件路径', default='test.csv')
     args = parser.parse_args()
     username = args.login
     type = args.type
+    url = args.url
     query = args.query
     keyword = args.keyword
+    export = args.export
     print(Wgpbanner())
-    WgpIG().Start(username, type, query, keyword)
+    WgpIG().Start(username, url, type, query, keyword, export)
 
 if __name__ == '__main__':
     main()
